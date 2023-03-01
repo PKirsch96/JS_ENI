289e221fe322ab610983f0a3ee598f686a0c67af
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-01 09:51:30 +0100

    paul

--- demoDOM/main.js
+++ demoDOM/main.js
@@ -9,7 +9,7 @@ btnBas.addEventListener("click", move);
 btnDroite.addEventListener("click", move);
 btnGauche.addEventListener("click", move);
 
-document.addEventListener("keypress", move)
+document.addEventListener("keypress", moveBtn)
 
 monCanvas.width = 1720;
 monCanvas.height = 880;
@@ -35,6 +35,21 @@ function move(evt) {
     ctx.drawImage(vaisseau, oldX, oldY);
 }
 
+function moveBtn(e) {
+    if (e.key === 'ArrowUp') {
+        move("btnHaut")
+    }
+    else if (e.key === 'ArrowDown') {
+        move("btnBas")
+    }
+    else if (e.key === 'ArrowLeft') {
+        move("btnGauche")
+    }
+    else if (e.key === 'ArrowRight') {
+        move("btnDroite")
+    }
+}
+
 
 const ctx = monCanvas.getContext('2d');
 
@@ -46,6 +61,52 @@ vaisseau.src = "images/spaceship32x32.png"
 vaisseau.onload = () => {
     ctx.drawImage(vaisseau, oldX, oldY)
 };
+
+
+// let bouger = (move) => {
+//     ctx.clearRect(oldX,oldY,32,32);
+//     switch (move){
+//         case 0:
+//             oldX += 32
+//             ctx.drawImage(vaisseau,oldX,oldY);
+//             break;
+//         case 1:
+//             oldX -= 32
+//             ctx.drawImage(vaisseau,oldX,oldY);
+//             break;
+//         case 2:
+//             oldY -= 32
+//             ctx.drawImage(vaisseau,oldX,oldY);
+//             break;
+//         case 3:
+//             oldY += 32
+//             ctx.drawImage(vaisseau,oldX,oldY);
+//             break;
+//     }
+//
+// }
+// let droite = () => {
+//     bouger(0)
+// }
+// let gauche = () => {
+//     bouger(1)
+// }
+// let haut = () => {
+//     bouger(2)
+// }
+// let bas = () => {
+//     bouger(3)
+// }
+//
+
+
+
+// const listenerActions = [haut, droite, gauche, bas]
+// let boutoninput = document.getElementsByClassName("btn");
+// for(let i = 0; i < listenerActions.length; i++){
+//     boutoninput[i].addEventListener('click', listenerActions[i]);
+// }
+
 
 
 
