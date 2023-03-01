9091a716ebc618b2dfae2d2897d0808315b8a9ea
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-01 09:44:44 +0100

    paul

--- demoDOM/main.js
+++ demoDOM/main.js
@@ -1,82 +1,51 @@
-let monCanvas = document.getElementById("monCanvas")
+const monCanvas = document.getElementById("monCanvas");
+const btnHaut = document.getElementById("btnHaut");
+const btnBas = document.getElementById("btnBas");
+const btnDroite = document.getElementById("btnDroite");
+const btnGauche = document.getElementById("btnGauche");
 
-monCanvas.width = 600;
-monCanvas.height = 400;
+btnHaut.addEventListener("click", move);
+btnBas.addEventListener("click", move);
+btnDroite.addEventListener("click", move);
+btnGauche.addEventListener("click", move);
+
+document.addEventListener("keypress", move)
+
+monCanvas.width = 1720;
+monCanvas.height = 880;
+
+const pas = 10;
+
+function move(evt) {
+    ctx.clearRect(oldX, oldY, 32, 32);
+    switch (evt.target.id) {
+        case "btnHaut" :
+            oldY -= pas;
+            break;
+        case "btnBas" :
+            oldY += pas;
+            break;
+        case "btnDroite" :
+            oldX += pas;
+            break;
+        case "btnGauche" :
+            oldX -= pas;
+            break;
+    }
+    ctx.drawImage(vaisseau, oldX, oldY);
+}
+
 
 const ctx = monCanvas.getContext('2d');
-//
-// ctx.font ="30px Arial";
-// ctx.fillStyle ="white";
-// ctx.fillText("ENI", 250,200);
-// // ctx.strokeText("ENI", 250,200);
-// ctx.fillStyle ="red";
-// ctx.fillRect(50, 50, 100, 80)
 
-let vaisseau = new Image(32,32)
+let oldX = monCanvas.width / 2 - 16;
+let oldY = monCanvas.height / 2 - 16;
+
+let vaisseau = new Image(32, 32)
 vaisseau.src = "images/spaceship32x32.png"
-vaisseau.onload = () => {ctx.drawImage(vaisseau,20,20)};
-
-let oldX = 20
-let oldY = 20
-let bouger = (move) => {
-    ctx.clearRect(oldX,oldY,32,32);
-    switch (move){
-        case 0:
-            oldX += 32
-            ctx.drawImage(vaisseau,oldX,oldY);
-            break;
-        case 1:
-            oldX -= 32
-            ctx.drawImage(vaisseau,oldX,oldY);
-            break;
-        case 2:
-            oldY -= 32
-            ctx.drawImage(vaisseau,oldX,oldY);
-            break;
-        case 3:
-            oldY += 32
-            ctx.drawImage(vaisseau,oldX,oldY);
-            break;
-    }
-
-}
-let droite = () => {
-    bouger(0)
-}
-let gauche = () => {
-    bouger(1)
-}
-let haut = () => {
-    bouger(2)
-}
-let bas = () => {
-    bouger(3)
-}
-
-
-document.onkeydown = checkKey;
-function checkKey(e) {
-    if (e.key === 'ArrowUp') {
-        bouger(2);
-    }
-    else if (e.key === 'ArrowDown') {
-        bouger(3);
-    }
-    else if (e.key === 'ArrowLeft') {
-        bouger(1);
-    }
-    else if (e.key === 'ArrowRight') {
-        bouger(0);
-    }
-}
-
-
-const listenerActions = [haut, droite, gauche, bas]
-let boutoninput = document.getElementsByClassName("btn");
-for(let i = 0; i < listenerActions.length; i++){
-    boutoninput[i].addEventListener('click', listenerActions[i]);
-}
-
+vaisseau.onload = () => {
+    ctx.drawImage(vaisseau, oldX, oldY)
+};
 
 
 
