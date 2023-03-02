8f0441c99fffaaf9173bc3ff8e555ff54c178616
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-02 14:56:32 +0100

    paul

--- demoDOM/POO/Poke.js
+++ demoDOM/POO/Poke.js
@@ -8,22 +8,44 @@ let pokemon = function (nom, numID, taille, poids, type, pdv, attaque1, attaque2
     this.pdv = pdv;
     this.attaque1 = attaque1;
     this.attaque2 = attaque2;
+    this.etat = true;
     this.afficher = () => {
         console.log(`
-        nom: ${nom},
-        ID: #${numID},
-        Taille: ${taille} m,
-        Poids: ${poids} kg,
-        Type: ${type},
-        Points de vie: \u2661 ${pdv},
-        Attaque: ${attaque1.nom}, dégats = ${attaque1.degats},
-        Attaque: ${attaque2.nom}, dégats = ${attaque2.degats},
+        nom: ${this.nom},
+        ID: #${this.numID},
+        Taille: ${this.taille} m,
+        Poids: ${this.poids} kg,
+        Type: ${this.type},
+        Points de vie: \u2661 ${this.pdv},
+        Attaque: ${this.attaque1.nom}, dégats = ${this.attaque1.degats} \ud83d\uddf2,
+        Attaque: ${this.attaque2.nom}, dégats = ${this.attaque2.degats} \ud83d\uddf2,
         `)
+    }
+    let pdv20 = this.pdv * 0.2;
+    this.attack = (adversaire) =>{
+        if(this.pdv < pdv20){
+            adversaire.pdv -= this.attaque2.degats;
+        }else{
+            adversaire.pdv -= this.attaque1.degats;
+        }
     }
 }
 
-let pikachu = new pokemon("Pikachu", 25, 0.4, 6, "éclaire", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
-let evoli = new pokemon("Evoli", 133, 0.4, 6, "éclaire", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
-pikachu.afficher()
+let combat = (adversaire1 , adversaire2) =>{
+    pikachu.afficher()
+    evoli.afficher()
+    while(adversaire1.pdv > 0 && adversaire2.pdv > 0){
+        adversaire1.attack(adversaire2)
+        adversaire2.attack(adversaire1)
+    }
+    pikachu.afficher()
+    evoli.afficher()
+}
 
 
+let pikachu = new pokemon("Pikachu", 25, 0.4, 6, "éclectrique", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
+let evoli = new pokemon("Evoli", 133, 0.3, 6.5, "normal", 70, {nom: "adaptabilité", degats: 9}, {nom: "anticipation", degats: 15});
+
+
+
+combat(pikachu, evoli)
