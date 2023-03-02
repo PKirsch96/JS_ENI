3b4a197294364abcae6ea23aaf34d5f84aea0011
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-02 13:10:33 +0100

    paul

--- demoDOM/POO/Poke.js
+++ demoDOM/POO/Poke.js
@@ -10,20 +10,20 @@ let pokemon = function (nom, numID, taille, poids, type, pdv, attaque1, attaque2
     this.attaque2 = attaque2;
     this.afficher = () => {
         console.log(`
-        ${nom},
-        ${numID},
-        ${taille},
-        ${poids},
-        ${type},
-        ${pdv},
-        attaque: ${attaque1.nom}, dégats = ${attaque1.degats},
-        attaque: ${attaque2.nom}, dégats = ${attaque2.degats},
+        nom: ${nom},
+        ID: #${numID},
+        Taille: ${taille} m,
+        Poids: ${poids} kg,
+        Type: ${type},
+        Points de vie: \u2661 ${pdv},
+        Attaque: ${attaque1.nom}, dégats = ${attaque1.degats},
+        Attaque: ${attaque2.nom}, dégats = ${attaque2.degats},
         `)
     }
 }
 
-let pikachu = new pokemon("Pikachu", 25, "0.4 m", "6 kg", "éclaire", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
+let pikachu = new pokemon("Pikachu", 25, 0.4, 6, "éclaire", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
+let evoli = new pokemon("Evoli", 133, 0.4, 6, "éclaire", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
 pikachu.afficher()
-
 
 
