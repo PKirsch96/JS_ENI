ebf35a5e03bc909de9de631cff058e1b35fc4298
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-02 15:14:33 +0100

    paul

--- demoDOM/POO/Poke.js
+++ demoDOM/POO/Poke.js
@@ -1,4 +1,3 @@
-
 let pokemon = function (nom, numID, taille, poids, type, pdv, attaque1, attaque2) {
     this.nom = nom;
     this.numID = numID;
@@ -22,31 +21,73 @@ let pokemon = function (nom, numID, taille, poids, type, pdv, attaque1, attaque2
         `)
     }
     let pdv20 = this.pdv * 0.2;
-    this.attack = (adversaire) =>{
-        if(this.pdv < pdv20){
+    this.attack = (adversaire) => {
+        if (this.pdv < pdv20) {
             adversaire.pdv -= this.attaque2.degats;
-        }else{
+        } else {
             adversaire.pdv -= this.attaque1.degats;
         }
     }
 }
 
-let combat = (adversaire1 , adversaire2) =>{
-    pikachu.afficher()
-    evoli.afficher()
-    while(adversaire1.pdv > 0 && adversaire2.pdv > 0){
-        adversaire1.attack(adversaire2)
-        adversaire2.attack(adversaire1)
-        pikachu.afficher()
-        evoli.afficher()
+let combat = (adversaire1, adversaire2) => {
+
+    let prems = Math.floor(Math.random() * 2);
+    console.log(prems)
+    if (prems === 1) {
+
+        adversaire1.afficher()
+        adversaire2.afficher()
+    } else {
+
+        adversaire2.afficher()
+        adversaire1.afficher()
+    }
+
+    while (adversaire1.pdv > 0 && adversaire2.pdv > 0) {
+        if (prems === 1) {
+
+            adversaire1.attack(adversaire2)
+            adversaire2.attack(adversaire1)
+        } else {
+
+            adversaire2.attack(adversaire1)
+            adversaire1.attack(adversaire2)
+        }
+
+        if (prems === 1) {
+
+            adversaire1.afficher()
+            adversaire2.afficher()
+        } else {
+
+            adversaire2.afficher()
+            adversaire1.afficher()
+        }
+        console.log(prems)
     }
 
 }
 
+let pikachu = new pokemon("Pikachu", 25, 0.4, 6, "éclectrique", 82,
+    {
+        nom: "Statik",
+        degats: 10
+    },
+    {
+        nom: "Paratonnerre",
+        degats: 25
+    });
 
-let pikachu = new pokemon("Pikachu", 25, 0.4, 6, "éclectrique", 82, {nom: "Statik", degats: 10}, {nom: "Paratonnerre", degats: 25});
-let evoli = new pokemon("Evoli", 133, 0.3, 6.5, "normal", 70, {nom: "adaptabilité", degats: 9}, {nom: "anticipation", degats: 15});
-
+let evoli = new pokemon("Evoli", 133, 0.3, 6.5, "normal", 70,
+    {
+        nom: "adaptabilité",
+        degats: 9
+    },
+    {
+        nom: "anticipation",
+        degats: 15
+    });
 
 
 combat(pikachu, evoli)
