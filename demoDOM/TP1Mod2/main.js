4b0c87e212b7db61f46e74005a64cad214118554
PKirsch96 <paul.kirsch96@gmail.com>
2023-03-02 17:24:44 +0100

    paul

--- demoDOM/TP1Mod2/main.js
+++ demoDOM/TP1Mod2/main.js
@@ -8,9 +8,12 @@ function showOnlisteAPuces(){
     let auteur = document.getElementById("auteur").value;
     let Date = document.getElementById("Date").value;
     let ligne = document.createElement("li");
+
     ligne.innerHTML = `<h1>${text} </h1> \nPar ${auteur}, le ${Date}` ;
+
     //listeAPuces = <ol>
     listeAPuces.appendChild(ligne);
+
     ligne.addEventListener("click", (event)=>{
         if(confirm(`Supprimer ${text} ?`)){
         event.currentTarget.remove();}
@@ -18,3 +21,4 @@ function showOnlisteAPuces(){
 }
 
 
+fetch()
